582442c55c1761cf06c646aff866f7a57dd765c9
Mohammad_AlAmin <alamincsecu@gmail.com>
2021-09-16 21:38:58 +0600

    added math func

--- hello_dart_project/bin/hello_dart_project.dart
+++ hello_dart_project/bin/hello_dart_project.dart
@@ -1,6 +1,12 @@
+import 'dart:math';
+
 void main() {
   print('Hello world!');
   print('dart apperentice');
   print("dart apperentice,,");
   print(22 / 6);
+  print(pi);
+  print(cos(90 * pi / 180));
+
+  //mathmathical function
 }
