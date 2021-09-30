a32d666b05233af72c2d3d270ce24b64837a48a4
Mohammad_AlAmin <alamincsecu@gmail.com>
2021-09-30 08:13:02 +0600

    Add input in dart

--- type_in_dart/bin/type_in_dart.dart
+++ type_in_dart/bin/type_in_dart.dart
@@ -1,7 +1,14 @@
+import 'dart:io';
+
 void main() {
   //print('Hello world!');
-  var firstName = "Mohammad";
-  String lastName = 'Al amin';
+  // var firstName = "Mohammad";
+  // String lastName = 'Al amin';
 
-  print(firstName + ' ' + lastName);
+  // print(firstName + ' ' + lastName);
+
+  ///data input
+  stdout.writeln('what is your first name: ? ');
+  String? name = stdin.readLineSync();
+  print('My name is $name');
 }
