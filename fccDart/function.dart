e63b9af7143e660eec187be2806520c9d1734311
Md. Al amin <alamincsecu@gmail.com>
2022-02-01 07:55:44 +0600

    Annonymus function

--- fccDart/function.dart
+++ fccDart/function.dart
@@ -1,12 +1,31 @@
 void main() {
-  output(square(10));
+  var names = ['Al amin', 'Khuku', 'Monjurul', 'Chamaly', 'Juthi', 'Suchi'];
+  
+  //output(square(10));
+  //names.forEach(printF)
+
+  ///Nameless or annonymus function
+  names.forEach((item) {
+    print(item);
+  });
 }
 
 ///Plain Function
-dynamic square(var num) {
-  return num * num;
-}
+// dynamic square(var num) {
+//   return num * num;
+// }
 
-void output(var msg) {
-  print(msg);
-}
+///Arrow function or fat arrow expression
+// dynamic square(var num) => num * num;
+
+// void output(var msg) {
+//   print(msg);
+//}
+
+///Annonymus or nameless function
+///Function without name
+
+///Named printF function
+// void printF(item) {
+//   print(item);
+// }
