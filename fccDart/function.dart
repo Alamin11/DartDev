8b78a42e8b060b408cfda4d24611836d809c46a6
Md. Al amin <alamincsecu@gmail.com>
2022-02-01 09:25:49 +0600

    parameter in function

--- fccDart/function.dart
+++ fccDart/function.dart
@@ -1,13 +1,18 @@
 void main() {
   var names = ['Al amin', 'Khuku', 'Monjurul', 'Chamaly', 'Juthi', 'Suchi'];
-  
+
   //output(square(10));
   //names.forEach(printF)
 
   ///Nameless or annonymus function
-  names.forEach((item) {
-    print(item);
-  });
+  // names.forEach((item) {
+  //   print(item);
+  // });
+  ///Positional parametered function
+  print(sum(10, 2)); //here 10 is taken as argument of num1 and 2 is for num2
+  print(sum2(num1: 20, num2: 2));
+  print(sum3(15));
+  print(sum4(25));
 }
 
 ///Plain Function
@@ -29,3 +34,22 @@ void main() {
 // void printF(item) {
 //   print(item);
 // }
+
+///Parametered function in dart programming
+///There are two types of parameters in dart
+///1. named / optional parameter
+///2. positional parameter
+
+//positional parametered function
+dynamic sum(var num1, var num2) => num1 + num2;
+
+///Now if we want to change the positonal parameter to named ,
+///we need to put a pair of curly braces sorrounding the parameter at declaration
+
+dynamic sum2({var num1, var num2}) => num1 + num2;
+
+/// If we want to make one optional and another positioned then
+
+dynamic sum3(var num2, {var num1}) =>
+    (num1 ?? 0) + num2; //using null aware operator
+dynamic sum4(var num1, [var num2 = 0]) => num1 + num2; // using default value
