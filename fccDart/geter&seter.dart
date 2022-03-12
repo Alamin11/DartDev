cad1ede54db253a1dc4184296282a8f1a0849d60
Md. Al amin <alamincsecu@gmail.com>
2022-03-12 15:31:31 +0600

    Getter and setter

---
+++ fccDart/geter&seter.dart
@@ -0,0 +1,17 @@
+void main(List<String> args) {
+  var rect1 = Rectangle(5, 7, 10, 9);
+  print(rect1.left);
+  rect1.right = 20;
+  print(rect1.left);
+}
+
+class Rectangle {
+  num left, top, width, height;
+  Rectangle(this.left, this.top, this.width, this.height);
+
+  num get right => left + width;
+  set right(num value) => left = value - width;
+
+  num get bottom => top + height;
+  set bottom(num value) => top = value - height;
+}
